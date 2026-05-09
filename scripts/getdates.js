31cc2bfd80030dd401e137be88a1f2e125b3e16c
FerAmadorSolano <fernanda.ama25@gmail.com>
2026-05-08 22:04:59 -0600

    home page

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+// Current year
+const year = new Date().getFullYear();
+
+document.getElementById("currentyear").textContent = year;
+
+// Last modified date
+document.getElementById("lastModified").innerHTML =
+    `Last Modified: ${document.lastModified}`;
